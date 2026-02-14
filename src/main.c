14fc54a718f92fa1167f8a6ad1a073b76c027552
Joshua Arsenault <jtarsenault86@gmail.com>
2026-02-14 18:54:51 +0200

    first changes

--- src/main.c
+++ src/main.c
@@ -8,8 +8,48 @@
 #include "parse.h"
 
 void print_usage(char *argv[]) {
+	printf("usage %s -n -f <database file>\n", argv[0]);
+	printf("\t -n -create new database file\n");
+	printf("\t -f (required) path to database file\n");
+	return;
 }
 
 int main(int argc, char *argv[]) { 
 	
+	char *filepath = NULL;
+	bool newfile = false;
+	int c;
+
+	while(c = getopt(argc, argv, "nf:") != -1) {
+
+		switch(c) {
+
+			case 'n':
+				newfile = true;
+				break;
+
+			case 'f':
+				filepath = optarg;
+				break;
+
+			case '?':
+				printf("Unknown flag: -%c\n");
+				break;
+
+			default:
+				return -1;
+		}
+	}
+
+	if (filepath == NULL) {
+		printf("Filepath is a required argument!\n");
+		print_usage(argv);
+	}
+
+
+	// just for testing
+	printf("newfile flag: %d\n", newfile);
+	printf("filepath: %s\n", filepath);
+
+	return 0;
 }
